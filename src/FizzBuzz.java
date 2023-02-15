b17edc8f60340d0e0795daa6d83ced2f20de6d82
hiroki-coffee <125346034+hiroki-coffee@users.noreply.github.com>
2023-02-16 02:32:35 +0900

    課題再提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,10 +5,16 @@ public class FizzBuzz {
 		// TODO 自動生成されたメソッド・スタブ
 		for (int counts = 1; counts <= 100; counts++) {
 			if (counts % 3 == 0 && counts % 5 == 0) {
-				System.out.println(counts + "=" +"FizzBuzz");}
+				System.out.println("FizzBuzz");
+				}
 				else if (counts % 3 == 0) {
-				System.out.println(counts + "=" +"Fizz");}
+				System.out.println("Fizz");
+				}
 				else if (counts % 5 == 0) {
-					System.out.println(counts + "=" +"Buzz");}
+					System.out.println("Buzz");
+					}
 				else
-			        System.out.println(counts + "=" + "none");}}}
+			        System.out.println(counts);
+			}
+		}
+	}
